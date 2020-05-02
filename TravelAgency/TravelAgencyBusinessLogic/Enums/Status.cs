b49ceb68f967ae5ab1107ee94083da304cba9c89
xamp13 <den.ohotnikov@gmail.com>
2020-05-02 23:27:55 +0400

    добавлен статус тура

---
+++ TravelAgency/TravelAgencyBusinessLogic/Enums/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelAgencyBusinessLog3ic.Enums
+{
+    public enum Status
+    {
+        Создан = 0,
+
+        Оформляется = 1,
+
+        Выполняется = 2,
+
+        Завершен = 3
+    }
+}
